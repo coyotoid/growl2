f02606b97b95c0bbf928f601f3cf4fd3f88d4bd2
Javier B. Torres <lobo@quiltro.org>
2026-04-21 15:14:03 -0400

    improve driver

--- bin/growl.ml
+++ bin/growl.ml
@@ -1,64 +1,106 @@
-open Containers
+open Cmdliner
 open Growl_core
-module String_map = Map.Make (String)
 
 let () = Fmt_tty.setup_std_outputs ~style_renderer:`Ansi_tty ()
-let db = Db.create ()
-let () = Theory.load_prelude db
-let test_file = if Array.length Sys.argv > 1 then Sys.argv.(1) else "test.grr"
-let fid = Query.FileId test_file
 
-let () =
+let file =
+  let doc = "The source file to process." in
+  Arg.(required & pos 0 (some non_dir_file) None & info [] ~docv:"FILE" ~doc)
+
+let setup_db file =
+  let db = Db.create () in
+  Theory.load_prelude db;
+  let fid = Query.FileId file in
   Db.store db (Query.SourceText fid)
-    (In_channel.with_open_text test_file In_channel.input_all)
+    (In_channel.with_open_text file In_channel.input_all)
     [];
-  Db.store db (Query.Manifest ()) [ fid ] []
+  Db.store db (Query.Manifest ()) [ fid ] [];
+  (db, fid)
 
-let print_text t = Fmt.pr "%a@." Text.pp t
-let prerr_text t = Fmt.epr "%a@." Text.pp t
-
-let coalesce t =
+let coalesce_ty t =
   let module C = Coalescing.Make () in
   t |> Compact_type.compact |> Compact_type.simplify |> C.coalesce
   |> Type.simplify_ty
 
-let main () =
-  let open Diagnosed.Syntax in
-  let program = Resolver.ask db (Query.ParsedProgram fid) in
-  if Diagnosed.has `Error program then Diagnosed.return ()
-  else
-    let* program = program in
+let exec_cmd =
+  let run file =
+    let db, fid = setup_db file in
+    let open Diagnosed.Syntax in
+    let program = Resolver.ask db (Query.ParsedProgram fid) in
+    if Diagnosed.has `Error program then Diagnosed.return ()
+    else
+      let* program = program in
+      let* types =
+        List.fold_right
+          (fun (def : Ast.def Span.Spanned.t) acc ->
+            let* acc = acc in
+            let* ty = Resolver.ask db (Query.WordType def.value.name.value) in
+            Diagnosed.return ((def.value.name.value, ty) :: acc))
+          program (Diagnosed.return [])
+      in
+      (* List.iter
+        (fun (name, ty) ->
+          Fmt.pr "%a@." Text.pp
+            Text.
+              [
+                Text "The type of ";
+                Verbatim name;
+                Text " is ";
+                Any (coalesce_ty ty, Type_pp.pp_ty);
+              ])
+        types; *)
+      let* main_ty = Resolver.ask db (Query.WordType "main") in
+      match Simple_type.is_error main_ty with
+      | true -> Diagnosed.return ()
+      | false ->
+          let stack = Interpret.exec program in
+          Fmt.epr "Resulting stack: @[%a@]@."
+            (Fmt.brackets (Fmt.list ~sep:Fmt.sp Interpret.pp_value))
+            (List.rev stack);
+          Diagnosed.return ()
+  in
+  let run file =
+    Reporting.with_reporting (run file) |> Diagnosed.run |> Pair.fst
+  in
+  let doc = "Type-check and run a source file." in
+  Cmd.v (Cmd.info "exec" ~doc) Term.(const run $ file)
 
-    (* List the type of all definitions *)
-    let* types =
-      List.fold_right
-        (fun (def : Ast.def Span.Spanned.t) acc ->
-          let* acc = acc in
-          let* ty = Resolver.ask db (Query.WordType def.value.name.value) in
-          Diagnosed.return ((def.value.name.value, ty) :: acc))
-        program (Diagnosed.return [])
-    in
+let check_cmd =
+  let run file =
+    let db, fid = setup_db file in
+    let open Diagnosed.Syntax in
+    let program = Resolver.ask db (Query.ParsedProgram fid) in
+    if Diagnosed.has `Error program then Diagnosed.return ()
+    else
+      let* program = program in
+      let* types =
+        List.fold_right
+          (fun (def : Ast.def Span.Spanned.t) acc ->
+            let* acc = acc in
+            let* ty = Resolver.ask db (Query.WordType def.value.name.value) in
+            Diagnosed.return ((def.value.name.value, ty) :: acc))
+          program (Diagnosed.return [])
+      in
+      Diagnosed.return
+      @@ List.iter
+           (fun (name, ty) ->
+             Fmt.pr "%a@." Text.pp
+               Text.
+                 [
+                   Text name;
+                   Text " :: ";
+                   Any (coalesce_ty ty, Type_pp.pp_ty);
+                 ])
+           types
+  in
+  let run file =
+    Reporting.with_reporting (run file) |> Diagnosed.run |> Pair.fst
+  in
+  let doc = "Type-check and print a source file's type definitions." in
+  Cmd.v (Cmd.info "check" ~doc) Term.(const run $ file)
 
-    List.iter
-      (fun (name, ty) ->
-        prerr_text
-          Text.
-            [
-              Text "The type of ";
-              Verbatim name;
-              Text " is ";
-              Any (coalesce ty, Type_pp.pp_ty);
-            ])
-      types;
+let cmd =
+  let doc = "an experimental stack-based language" in
+  Cmd.group (Cmd.info "growl" ~doc) [ exec_cmd; check_cmd ]
 
-    let* main_ty = Resolver.ask db (Query.WordType "main") in
-    match Simple_type.is_error main_ty with
-    | true -> Diagnosed.return ()
-    | false ->
-        let stack = Interpret.exec program in
-        Fmt.epr "Resulting stack: @[%a@]@."
-          (Fmt.brackets (Fmt.list ~sep:Fmt.sp Interpret.pp_value))
-          stack;
-        Diagnosed.return ()
-
-let _ = Reporting.with_reporting (main ())
+let () = exit (Cmd.eval cmd)
