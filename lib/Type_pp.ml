cb9d4c456075d661b04e7276d8a89e1e1656a34e
Javier B. Torres <lobo@quiltro.org>
2026-04-22 12:48:56 -0400

    fix some discrepancies with type printing

--- lib/Type_pp.ml
+++ lib/Type_pp.ml
@@ -12,7 +12,10 @@ let rec pp_ty : ty Fmt.t =
       (Fmt.parens (Fmt.pair ~sep:(Fmt.any "@ & ") pp_ty pp_ty)) ppf (a, b)
   | TFunc (a, b) ->
       (Fmt.parens (Fmt.pair ~sep:(Fmt.any "@ -> ") pp_stack pp_stack)) ppf (a, b)
-  | TRec { name; body } -> Fmt.pf ppf "𝜇%s. %a" name pp_ty body
+  | TRec { name; body } ->
+      Fmt.pair ~sep:Fmt.sp
+        Fmt.(any "μ" ++ string ++ any ".")
+        pp_ty ppf (name, body)
   | TVar v -> Fmt.string ppf v
   | TPrim `Nat -> Fmt.string ppf "nat"
   | TPrim `Int -> Fmt.string ppf "int"
@@ -31,10 +34,13 @@ and pp_stack : stack Fmt.t =
   | SBot -> Fmt.string ppf "⊥"
   | STop -> Fmt.string ppf "⊤"
   | SUnion (a, b) ->
-      (Fmt.parens (Fmt.pair ~sep:(Fmt.any " |@ ") pp_stack pp_stack)) ppf (a, b)
+      (Fmt.parens (Fmt.pair ~sep:(Fmt.any "@ | ") pp_stack pp_stack)) ppf (a, b)
   | SInter (a, b) ->
-      (Fmt.parens (Fmt.pair ~sep:(Fmt.any " &@ ") pp_stack pp_stack)) ppf (a, b)
-  | SRec { name; body } -> Fmt.pf ppf "𝜇%s. %a" name pp_stack body
+      (Fmt.parens (Fmt.pair ~sep:(Fmt.any "@ & ") pp_stack pp_stack)) ppf (a, b)
+  | SRec { name; body } ->
+      Fmt.pair ~sep:Fmt.sp
+        Fmt.(any "μ" ++ string ++ any ".")
+        pp_stack ppf (name, body)
   | SVar v -> Fmt.(any ".." ++ string) ppf v
   | SCons (t, s) -> Fmt.pair ~sep:Fmt.comma pp_stack pp_ty ppf (s, t)
 
